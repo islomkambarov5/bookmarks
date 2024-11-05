182d4cec5e264d79fca46415da3908ddcc834c3a
islomkambarov5 <islomkambarov5@gmail.com>
2024-11-05 21:39:39 +0500

    updates

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -146,4 +146,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# Your email data
+EMAIL_HOST_USER = 'islomkambarov5@gmail.com'
+EMAIL_HOST_PASSWORD = 'oktr byff rquk inuo'
